3876c9283d12daf0a8e1436d122ee85cdc9011e7
Jaehong Kang <sinoru@me.com>
2023-02-26 14:01:24 +0900

    Updates for older swift

--- Sources/TopLevelCoder/TopLevelDecoder.swift
+++ Sources/TopLevelCoder/TopLevelDecoder.swift
@@ -8,6 +8,7 @@
 #if canImport(Combine)
 import Combine
 
+#if swift(>=5.4)
 public protocol TopLevelDecoder<Input>: Combine.TopLevelDecoder {
     /// The type this decoder accepts.
     associatedtype Input
@@ -16,6 +17,18 @@ public protocol TopLevelDecoder<Input>: Combine.TopLevelDecoder {
     func decode<T>(_ type: T.Type, from: Self.Input) throws -> T where T: Decodable
 }
 #else
+public protocol TopLevelDecoder: Combine.TopLevelDecoder {
+    /// The type this decoder accepts.
+    associatedtype Input
+
+    /// Decodes an instance of the indicated type.
+    func decode<T>(_ type: T.Type, from: Self.Input) throws -> T where T: Decodable
+}
+#endif
+
+#else
+
+#if swift(>=5.4)
 public protocol TopLevelDecoder<Input> {
     /// The type this decoder accepts.
     associatedtype Input
@@ -23,4 +36,14 @@ public protocol TopLevelDecoder<Input> {
     /// Decodes an instance of the indicated type.
     func decode<T>(_ type: T.Type, from: Self.Input) throws -> T where T: Decodable
 }
+#else
+public protocol TopLevelDecoder {
+    /// The type this decoder accepts.
+    associatedtype Input
+
+    /// Decodes an instance of the indicated type.
+    func decode<T>(_ type: T.Type, from: Self.Input) throws -> T where T: Decodable
+}
+#endif
+
 #endif
--- Sources/TopLevelCoder/TopLevelEncoder.swift
+++ Sources/TopLevelCoder/TopLevelEncoder.swift
@@ -8,6 +8,7 @@
 #if canImport(Combine)
 import Combine
 
+#if swift(>=5.4)
 public protocol TopLevelEncoder<Output>: Combine.TopLevelEncoder {
     /// The type this encoder produces.
     associatedtype Output
@@ -18,6 +19,20 @@ public protocol TopLevelEncoder<Output>: Combine.TopLevelEncoder {
     func encode<T>(_ value: T) throws -> Self.Output where T: Encodable
 }
 #else
+public protocol TopLevelEncoder: Combine.TopLevelEncoder {
+    /// The type this encoder produces.
+    associatedtype Output
+
+    /// Encodes an instance of the indicated type.
+    ///
+    /// - Parameter value: The instance to encode.
+    func encode<T>(_ value: T) throws -> Self.Output where T: Encodable
+}
+#endif
+
+#else
+
+#if swift(>=5.4)
 public protocol TopLevelEncoder<Output> {
     /// The type this encoder produces.
     associatedtype Output
@@ -27,4 +42,16 @@ public protocol TopLevelEncoder<Output> {
     /// - Parameter value: The instance to encode.
     func encode<T>(_ value: T) throws -> Self.Output where T: Encodable
 }
+#else
+public protocol TopLevelEncoder {
+    /// The type this encoder produces.
+    associatedtype Output
+
+    /// Encodes an instance of the indicated type.
+    ///
+    /// - Parameter value: The instance to encode.
+    func encode<T>(_ value: T) throws -> Self.Output where T: Encodable
+}
+#endif
+
 #endif
