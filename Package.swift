acb2fec635814b124ea1f7320321d8c0216aff48
Jaehong Kang <sinoru@me.com>
2023-02-26 14:06:07 +0900

    Fixes for SwiftLint

--- Package.swift
+++ Package.swift
@@ -9,13 +9,13 @@ let package = Package(
         .macOS(.v10_15),
         .iOS(.v13),
         .watchOS(.v6),
-        .tvOS(.v13),
+        .tvOS(.v13)
     ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "TopLevelCoder",
-            targets: ["TopLevelCoder"]),
+            targets: ["TopLevelCoder"])
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -26,6 +26,6 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "TopLevelCoder",
-            dependencies: []),
+            dependencies: [])
     ]
 )
