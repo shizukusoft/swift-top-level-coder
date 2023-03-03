5683f078c260d15c928eea463de5f8067dc237df
Jaehong Kang <sinoru@me.com>
2023-03-03 21:01:18 +0900

    Renaming names
    Fixes #2

--- Sources/TopLevelCodersFoundationSupport/Foundation.PropertyListDecoder.swift
+++ Sources/TopLevelCodersFoundationSupport/Foundation.PropertyListDecoder.swift
@@ -6,7 +6,7 @@
 //
 
 import Foundation
-import TopLevelCoder
+import TopLevelCoders
 
 extension PropertyListDecoder: TopLevelDecoder {
 }
--- Sources/TopLevelCodersFoundationSupport/Foundation.PropertyListEncoder.swift
+++ Sources/TopLevelCodersFoundationSupport/Foundation.PropertyListEncoder.swift
@@ -6,7 +6,7 @@
 //
 
 import Foundation
-import TopLevelCoder
+import TopLevelCoders
 
 extension PropertyListEncoder: TopLevelEncoder {
 }
